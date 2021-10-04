be70001970e440c92fb0a18eff0ee3a1692b15dc
Vishal Maurya <vishal.maurya@atriina.com>
2021-10-04 10:32:10 +0530

    koan12 done

--- koans/about_strings.py
+++ koans/about_strings.py
@@ -23,7 +23,7 @@ class AboutStrings(Koan):
 
     def test_raw_strings_are_also_strings(self):
         string = r"Konnichi wa, world!"
-        self.assertEqual(__, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_use_single_quotes_to_create_string_with_double_quotes(self):
         string = 'He said, "Go Away."'
